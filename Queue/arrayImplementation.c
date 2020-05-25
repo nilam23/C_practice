aba8b9672279c456bbab9e399551495f6b8e5a0a
Nilam Pathak <nilampathak72@gmail.com>
2020-05-25 20:42:04 +0530

    array implementation of all basic queue operations

---
+++ Queue/arrayImplementation.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+
+int queue[MAX];
+int front = -1;
+int rear = -1;
+
+void insert(int item){
+	if(rear == MAX-1){
+		printf("Queue overflow.\n");
+		return;
+	}
+	if(front == -1)
+		front = 0;
+	queue[++rear] = item;
+	return;
+}
+
+int delete(){
+	if(front == -1 || front == rear+1){
+		printf("Queue underflow.\n");
+		return 0;
+	}
+	int temp = queue[front++];
+	return temp;
+}
+
+int peek(){
+	if(front == -1 || front == rear+1){
+		printf("Queue underflow.\n");
+		return 0;
+	}
+	return queue[front];
+}
+
+void display(){
+	if(front == -1 || front == rear+1){
+		printf("Queue underflow.\n");
+		return;
+	}
+	printf("The queue elements are: ");
+	for(int i=front; i<=rear; i++)
+		printf("%d\t", queue[i]);
+	printf("\n");
+}
+
+int main(){
+	int choice, item;
+	while(1){
+		printf("\n--PICK YOUR CHOICE--\n");
+		printf("1. Insert.\n");
+		printf("2. Delete.\n");
+		printf("3. Display the front element.\n");
+		printf("4. Display the elements in the queue.\n");
+		printf("5. Quit.\n");
+		printf("\nYour choice? ");
+		scanf("%d", &choice);
+		printf("\n");
+		switch(choice){
+			case 1:
+				printf("Enter the element to be inserted: ");
+				scanf("%d", &item);
+				insert(item);
+				break;
+			case 2:
+				item = delete();
+				if(item == 0)
+					break;
+				printf("The deleted element is: %d\n", item);
+				break;
+			case 3:
+				item = peek();
+				if(item == 0)
+					break;
+				printf("The element at the front is: %d\n", item);
+				break;
+			case 4:
+				display();
+				break;
+			case 5:
+				printf("\n--EXITING--\n");
+				exit(0);
+			default:
+				printf("Wrong choice. Re-enter again.\n");
+				break;
+		}
+	}
+	return 0;
+}
