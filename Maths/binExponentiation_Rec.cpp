0d6629a6fcf2c0e057d27b0ef90b063224582083
Nilam Pathak <nilampathak72@gmail.com>
2020-06-08 14:36:10 +0530

    recursive binary exponentiation (modified)

--- Maths/binExponentiation_Rec.cpp
+++ Maths/binExponentiation_Rec.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#define ll long long int
 using namespace std;
-int power(int a, int n){
+ll power(int a, int n){
 	if(n == 0)
 		return 1;
-	int result = power(a, n/2) * power(a, n/2);
+	ll result = power(a, n/2) * power(a, n/2);
 	if(n & 1)
 		return result * a;
 	else
