9129ed1bffd3aca4a769d6b4f333f67e30b4361c
Nilam Pathak <nilampathak72@gmail.com>
2020-06-15 13:05:25 +0530

    macro vs func

---
+++ Macro_VS_Function/function.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int num(){ return 10; }
+int main(){
+	printf("%d\n", num());
+	return 0;
+}
---
+++ Macro_VS_Function/macro.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#define num 10
+int main(){
+	printf("%d\n", num);
+	return 0;
+}
